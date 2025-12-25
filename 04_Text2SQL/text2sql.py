93c62101c39979cb481f94248ed49aea90a80ee2
DoounOh <dooun.oh@kurlycorp.com>
2025-12-25 09:45:28 +0900

    add: text2sql

---
+++ 04_Text2SQL/text2sql.py
@@ -0,0 +1,16 @@
+
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def text2sql(text):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": text}]
+    )
